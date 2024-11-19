17a119d0e0e5ea66c592b074c3064142150d53e1
PLucenaa <pedro12brasil@gmail.com>
2024-11-19 10:05:12 -0400

    criação e implementação de questionario.dart e resposta.dart

---
+++ lib/resultado.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Fantastico',
+        style: TextStyle(
+          fontSize: 35,
+        ),
+      ),
+    );
+  }
+}
